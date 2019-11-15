8a7770668a9de8a79fcb9fe026fb67d03e971d99
Joonas Nikolai Pollari <joonas.n.pollari@student.jyu.fi>
2019-11-15 23:32:55 +0200

    Created base for the movement of the snake's head

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class Head : MonoBehaviour
 {
+    
+    private float dir = 10.0f;
+    private Vector3 pytdir3;
+
+    [SerializeField]
+    private Transform trans;
+    
+    [SerializeField]
+    private float speed = 0.02f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pytdir3 = new Vector3(dir, dir, 0);
     }
 
-    public float speed = 1;
-
+    
     void Update()
     {
-        gameObject.GetComponent<Transform>().Translate(Vector2.up * Time.deltaTime * speed);
+        dir -= Input.GetAxis("Horizontal");
+        transform.eulerAngles = new Vector3(0, 0, 2* dir);
+
+        pytdir3.x = dir;
+        pytdir3.y = dir;
+        transform.Translate(pytdir3 * Time.deltaTime * speed / dir);
+
+
     }
 }
