1a5485341255917ec69384df11e25c95e4338922
Joonas Nikolai Pollari <joonas.n.pollari@student.jyu.fi>
2019-11-15 21:57:31 +0200

    Added folders for scripts and sprites

---
+++ Assets/Scripts/Head.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 1;
+
+    void Update()
+    {
+        gameObject.GetComponent<Transform>().Translate(Vector2.up * Time.deltaTime * speed);
+    }
+}
